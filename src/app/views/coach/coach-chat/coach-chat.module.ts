e2ceacec34fc25b238a19fe95a0e767909413c99
zied-23 <zied.19965@gmail.com>
2023-08-02 02:42:58 +0100

    02/08

--- src/app/views/coach/coach-chat/coach-chat.module.ts
+++ src/app/views/coach/coach-chat/coach-chat.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CoachChatRoutingModule } from './coach-chat-routing.module';
 import { CoachChatComponent } from './coach-chat/coach-chat.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    CoachChatComponent
-  ],
-  imports: [
-    CommonModule,
-    CoachChatRoutingModule
-  ]
+  declarations: [CoachChatComponent],
+  imports: [CommonModule, CoachChatRoutingModule, FormsModule],
 })
-export class CoachChatModule { }
+export class CoachChatModule {}
--- src/app/views/coach/coach-chat/coach-chat/coach-chat.component.ts
+++ src/app/views/coach/coach-chat/coach-chat/coach-chat.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { ChatService } from 'src/app/MesServices/Chat/chat.service';
 import { GroupService } from 'src/app/MesServices/Groups/group.service';
+import { UserService } from 'src/app/MesServices/UserService/user-service.service';
 import { UserAuthService } from 'src/app/MesServices/user-auth.service';
 import { Groups } from 'src/app/Models/group.model';
 
@@ -9,7 +12,7 @@ import { Groups } from 'src/app/Models/group.model';
   styleUrls: ['./coach-chat.component.css'],
 })
 export class CoachChatComponent {
-  groups: Groups[] = [];
+  /*  groups: Groups[] = [];
 
   constructor(
     private groupsService: GroupService,
@@ -22,5 +25,89 @@ export class CoachChatComponent {
       .subscribe((groups) => {
         this.groups = groups;
       });
+  }*/
+  groups: Groups[] = [];
+  selectedGroup: Groups | null = null;
+  chatMessages: any[] = [];
+  userFirstName$!: Observable<string>;
+  newMessage: string = '';
+  private jwtToken: string;
+  message: any;
+  isMessageLoaded = false;
+  @ViewChild('chatContainer') chatContainer!: ElementRef;
+  constructor(
+    private chatService: ChatService, // Use the ChatService
+    private groupsService: GroupService,
+    public userAuthService: UserAuthService,
+    private userService: UserService
+  ) {
+    this.jwtToken = localStorage.getItem('jwtToken') || '';
+  }
+
+  ngOnInit() {
+    if (this.jwtToken) {
+      const formateurid = this.userAuthService.getId();
+      this.groupsService
+        .getGroupsByFormateurId(formateurid)
+        .subscribe((groups) => {
+          this.groups = groups;
+          console.log('Fetched groups:', this.groups);
+          this.groups.forEach((group) => {
+            this.chatService
+              .getGroupLastMessage(group.id, formateurid)
+              .subscribe((lastMessage) => {
+                group.lastMessage = lastMessage || {};
+
+                this.chatService
+                  .getUnreadMessageCount(group.id, formateurid)
+                  .subscribe((unreadCount) => {
+                    group.unreadCount = unreadCount;
+                  });
+              });
+          });
+        });
+    } else {
+      console.log('No user connected');
+    }
+  }
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+  private scrollToBottom(): void {
+    if (this.chatContainer && this.chatContainer.nativeElement) {
+      const container = this.chatContainer.nativeElement;
+      container.scrollTop = container.scrollHeight;
+    }
+  }
+
+  onSelectGroup(group: Groups) {
+    console.log('Selected Group:', group);
+    if (group) {
+      this.selectedGroup = group;
+      this.chatMessages = [];
+      this.chatService.getGroupChat(group.id).subscribe((messages) => {
+        this.chatMessages = messages.map((message: any) => ({
+          ...message,
+          senderFirstName$: this.userService
+            .getUserById(message.senderId)
+            .pipe(map((user: any) => user.firstName)),
+        }));
+        setTimeout(() => {
+          this.scrollToBottom();
+        }, 0);
+      });
+    }
+  }
+
+  sendMessage() {
+    if (this.selectedGroup && this.newMessage.trim() !== '') {
+      const formateurid = this.userAuthService.getId();
+      this.chatService.sendMessage(
+        this.selectedGroup.id,
+        this.newMessage,
+        formateurid
+      );
+      this.newMessage = '';
+    }
   }
 }
