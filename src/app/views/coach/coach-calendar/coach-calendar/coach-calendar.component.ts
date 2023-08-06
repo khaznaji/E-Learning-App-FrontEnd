8be6bed9eb9acb47552f9ba94da56e647bd1e509
zied-23 <zied.19965@gmail.com>
2023-08-06 06:19:10 +0100

    Display groups for sessions '06/08'

--- src/app/views/coach/coach-calendar/coach-calendar/coach-calendar.component.ts
+++ src/app/views/coach/coach-calendar/coach-calendar/coach-calendar.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GroupService } from 'src/app/MesServices/Groups/group.service';
 import { SessionService } from 'src/app/MesServices/Session/session.service';
 import { UserAuthService } from 'src/app/MesServices/user-auth.service';
 import { Session } from 'src/app/Models/Session';
+import { Groups } from 'src/app/Models/group.model';
 
 @Component({
   selector: 'app-coach-calendar',
@@ -11,14 +14,29 @@ import { Session } from 'src/app/Models/Session';
 export class CoachCalendarComponent {
   sessions: Session[] = [];
   selectedSession!: Session;
+  sessionGroups: Groups[] = [];
 
   constructor(
     private sessionService: SessionService,
-    private userAuthService: UserAuthService
+    private userAuthService: UserAuthService,
+    private groupService: GroupService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+   
     this.retrieveSessions();
+    this.route.queryParams.subscribe((params) => {
+      const sessionId = Number(params['sessionId']);
+      if (!isNaN(sessionId) && sessionId > 0) {
+        this.selectSessionById(sessionId);
+      } else {
+        // If no valid sessionId in queryParams, select the first session
+        if (this.sessions.length > 0) {
+          this.selectSession(this.sessions[0]);
+        }
+      }
+    });
   }
 
   retrieveSessions(): void {
@@ -26,7 +44,16 @@ export class CoachCalendarComponent {
     this.sessionService.getSessionsByFormateurId(formateurId).subscribe(
       (sessions: Session[]) => {
         this.sessions = sessions;
-        this.selectedSession = sessions[0];
+        if (this.selectedSession && this.selectedSession.id !== undefined) {
+          this.getGroupsForSession(this.selectedSession.id);
+        } else if (sessions.length > 0) {
+          this.selectedSession = sessions[0];
+          if (this.selectedSession.id !== undefined) {
+            this.getGroupsForSession(this.selectedSession.id);
+          }
+        } else {
+          this.sessionGroups = [];
+        }
         console.log('Sessions:', sessions);
       },
       (error) => {
@@ -34,14 +61,42 @@ export class CoachCalendarComponent {
       }
     );
   }
+  
 
   selectSession(session: Session) {
     this.selectedSession = session;
+    if (session?.id) {
+      this.getGroupsForSession(session.id);
+    } else {
+      this.sessionGroups = []; 
+    }
   }
+  selectSessionById(sessionId: number | undefined) {
+    if (sessionId === undefined) {
+      return;
+    }
+    const session = this.sessions.find((s) => s.id === sessionId);
+    if (session) {
+      this.selectedSession = session;
+      this.getGroupsForSession(session.id as number); 
+    }
+  }
+
   calculateDuration(session: Session): number {
     const start = new Date(session.startDate);
     const finish = new Date(session.finishDate);
     const duration = (finish.getTime() - start.getTime()) / (1000 * 60);
     return duration;
+  }
+  getGroupsForSession(sessionId: number): void {
+    this.groupService.getGroupsBySessionId(sessionId).subscribe(
+      (groups: Groups[]) => {
+        this.sessionGroups = groups;
+        console.log('Groups for session:', groups);
+      },
+      (error) => {
+        console.log('Error retrieving groups:', error);
+      }
+    );
   }
 }
