c6bcbfe55893c7a4d62c9b9eb4415bb488d5c723
zied-23 <zied.19965@gmail.com>
2023-08-08 02:52:57 +0100

    Chaning rules firbase

--- src/environement/environement.dev.ts
+++ src/environement/environement.dev.ts
@@ -3,13 +3,13 @@ export const environement = {
   websocketUrl: 'http://localhost:8081/ws',
   production: false,
   firebase: {
-    apiKey: 'AIzaSyABkFa-9Q7FyzNz_jNjqubaFYy0E8h9OOM',
-    authDomain: 'groups-elearning-chat.firebaseapp.com',
-    projectId: 'groups-elearning-chat',
-    databaseURL: 'https://groups-elearning-chat-default-rtdb.firebaseio.com',
-    storageBucket: 'groups-elearning-chat.appspot.com',
-    messagingSenderId: '188747152226',
-    appId: '1:188747152226:web:ec5e728c6ac883be6eca3b',
-    measurementId: 'G-JZFV4E7DY7',
+    apiKey: "AIzaSyD6IDfMDyZTn5TbBu6mdVxo8eoeQrA_ErE",
+    authDomain: "elearning-groups-chat.firebaseapp.com",
+    databaseURL: "https://elearning-groups-chat-default-rtdb.europe-west1.firebasedatabase.app",  
+    projectId: "elearning-groups-chat", 
+    storageBucket: "elearning-groups-chat.appspot.com",  
+    messagingSenderId: "857202140084", 
+    appId: "1:857202140084:web:afb50acf20b283eb4d08e1", 
+    measurementId: "G-4GDBMLSBD4"
   },
 };
